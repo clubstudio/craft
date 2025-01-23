92155071f2e4a1896286685785b85f953de08a12
Scott Wakefield <scott@clubstudio.co.uk>
2025-01-23 13:51:32 +0000

    Add initial templates

--- config/general.php
+++ config/general.php
@@ -12,15 +12,12 @@ use craft\config\GeneralConfig;
 use craft\helpers\App;
 
 return GeneralConfig::create()
-    // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
     ->defaultWeekStartDay(1)
-    // Prevent generated URLs from including "index.php"
     ->omitScriptNameInUrls()
-    // Preload Single entries as Twig variables
     ->preloadSingles()
-    // Prevent user enumeration attacks
     ->preventUserEnumeration()
-    // Set the @webroot alias so the clear-caches command knows where to find CP resources
+    ->enableGql(false)
+    ->errorTemplatePrefix('_errors/')
     ->aliases([
         '@webroot' => dirname(__DIR__) . '/web',
         '@svg' => dirname(__DIR__) . '/src/svg',
