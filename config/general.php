255bb69b2712ca0f1a47dc8961f5c9b590b2f9e1
Scott Wakefield <scott@clubstudio.co.uk>
2025-01-23 11:28:58 +0000

    Add SVG alias

--- config/general.php
+++ config/general.php
@@ -23,5 +23,6 @@ return GeneralConfig::create()
     // Set the @webroot alias so the clear-caches command knows where to find CP resources
     ->aliases([
         '@webroot' => dirname(__DIR__) . '/web',
+        '@svg' => dirname(__DIR__) . '/resources/svg',
     ])
 ;
