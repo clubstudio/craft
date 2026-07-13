bdea6ea3afb6584067eeff4f0c13acb0c53fb66a
Scott Wakefield <scott@clubstudio.co.uk>
2026-07-13 15:00:33 +0100

    Update formie.php

--- config/formie.php
+++ config/formie.php
@@ -2,4 +2,14 @@
 
 return [
     'pluginName' => 'Forms',
+    'defaultPage' => 'submissions',
+
+    // Submissions
+    'useQueueForNotifications' => true,
+    'useQueueForIntegrations' => true,
+    'queuePriority' => 10,
+
+    // PDFs
+    'pdfPaperSize' => 'A4',
+    'pdfPaperOrientation' => 'portrait',
 ];
